e4f8dcbf62f24ecf40b0866779a106b637bff3b4
Seyyed Ali Mahdi <43001579+SeyyedAliMehdi@users.noreply.github.com>
2022-04-23 16:10:28 +0430

    Add files via upload

---
+++ Algorithms/Warm Up/Mini-Max Sum/Mini-Max Sum.js
@@ -0,0 +1,54 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+    // Write your code here
+    let maxValue = Number.MIN_VALUE
+let minValue = Number.MAX_VALUE
+let sum = 0
+
+
+for (const i of arr) {
+    if (i > maxValue){
+        maxValue = i
+    }
+    if (i < minValue){
+        minValue = i
+    }
+    sum += i
+}
+console.log(sum - maxValue, sum - minValue)
+
+}
+
+function main() {
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
